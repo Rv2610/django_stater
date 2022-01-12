20d36e2d83d95ce667b14a2af0039f473391aa47
rohit <vrrathod026@gmail.com>
2022-01-12 19:39:52 +0530

    added static and nav bar without img

--- durvesh/settings.py
+++ durvesh/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home'
+    
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,'durvesh/static')
+]
+
+SATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 
